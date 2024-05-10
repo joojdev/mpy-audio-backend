5d52d4ac9978a01000cd1b2747c08cb591dd5837
joojdev <jvpiovezan2905@gmail.com>
2024-05-10 14:34:37 -0300

    removi emojis do áudio e melhorei-o um pouco

--- audio.py
+++ audio.py
@@ -1,5 +1,6 @@
 from espeakng import ESpeakNG
 from pydub import AudioSegment
+from pydub.effects import low_pass_filter, high_pass_filter
 from io import BytesIO
 
 def prepare_audio(content):
@@ -14,6 +15,8 @@ def prepare_audio(content):
   audio = AudioSegment.from_wav(audio)
 
   audio = audio.set_frame_rate(8000).set_channels(1).set_sample_width(1)
+  low_pass_filter(audio, 3000)
+  high_pass_filter(audio, 300)
 
   audio += 5
 
--- gpt.py
+++ gpt.py
@@ -2,9 +2,31 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 from audio import prepare_audio
+import re
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def demojify(text):
+  # Regex para combinar emojis
+  emoji_pattern = re.compile(
+    "["
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F300-\U0001F5FF"  # símbolos & pictogramas
+    "\U0001F680-\U0001F6FF"  # transporte & mapas
+    "\U0001F700-\U0001F77F"  # alquimia
+    "\U0001F780-\U0001F7FF"  # Geometric shapes extended
+    "\U0001F800-\U0001F8FF"  # Supplemental arrows-C
+    "\U0001F900-\U0001F9FF"  # suplementar símbolos e pictogramas
+    "\U0001FA00-\U0001FA6F"  # Chess symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "\U000024C2-\U0001F251" 
+    "\U0000200D"        # zero width joiner
+    "\U0000FE0F"        # variation selector
+    "]+", flags=re.UNICODE)
+  
+  return emoji_pattern.sub(r'', text)
 
 def generate_speech_answer(prompt):
   text_response = client.chat.completions.create(
@@ -20,6 +42,6 @@ def generate_speech_answer(prompt):
   content = text_response.choices[0].message.content
   print(f' GPT > {content}')
 
-  audio_file = prepare_audio(content)
+  audio_file = prepare_audio(demojify(content))
 
   return audio_file, text_response.usage
